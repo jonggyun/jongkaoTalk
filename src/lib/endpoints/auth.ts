4c82699dc29e07ebfdc4cfb3493b65a90aa43921
JongGyun Park <jonggyuni@gmail.com>
2019-04-29 00:48:32 +0900

    Use crypto for user account

--- src/lib/endpoints/auth.ts
+++ src/lib/endpoints/auth.ts
@@ -1,4 +1,6 @@
 import { firebaseDB } from '../firebase';
+import CryptoJS from 'crypto-js';
+import { KEY } from '../../constant';
 
 const userRegister = async ({
   username,
@@ -10,11 +12,19 @@ const userRegister = async ({
   password: string;
 }) => {
   try {
-    console.log('userSignUp', username, nickname, password);
+    // const bytes = CryptoJS.AES.decrypt(
+    //   mypassword.toString(),
+    //   KEY.CRYPTO_MY_SECRET_KEY
+    // );
+    // const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+    // console.log('decrypto', decryptedData);
     firebaseDB.ref(`account/${username}`).set({
       username,
       nickname,
-      password,
+      password: CryptoJS.AES.encrypt(
+        password,
+        KEY.CRYPTO_MY_SECRET_KEY
+      ).toString(),
     });
   } catch (e) {
     console.log('error');
